790d414ddc62f2d2d1e30fb4151e7c161c7edf83
tamnt <tamnguyenthanh@admicro.vn>
2016-09-23 16:51:53 +0700

    Sửa để mỗi contact trên 1

--- src/main/java/functions/Utils.java
+++ src/main/java/functions/Utils.java
@@ -4,7 +4,7 @@ public class Utils {
 	public static String generateRow(String blog_url, String author_name, String tag, String contact)
 	{
 		String row = "";
-		contact = contact.replace("[", "").replace("]", "").replace(",", ";");
+		contact = contact.replace("[", "").replace("]", "");
 		row += blog_url + ", ";
 		row += author_name + ", ";
 		row += tag + ", ";
